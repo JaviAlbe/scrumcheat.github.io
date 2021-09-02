46f1b1eb4eda77f6c289b7a7ac512c0d3e605cd2
Albeniz <j.albenizlopez@gmail.com>
2021-09-02 17:18:15 +0200

    Successfully fetched a json of jokes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import "./app.css"
 import Header from "../header/Header"
 import Footer from "../footer/Footer"
@@ -7,38 +7,60 @@ import listOfUsers from "../../listOfUsers"
 
 function App() {
 
+    const[joke, setJoke] = useState("This is a placeholder for a joke")
+
+    useEffect(() => {
+
+        const url = "http://api.icndb.com/jokes/"
+
+        const fetchData = async () => {
+            const response = await fetch(url)
+            const json = await response.json()
+            console.log(json)
+            setJoke(json.value[1].joke)
+        }
+        fetchData()
+    }, [])
+
+
     const [listOfPeople, setListOfPeople] = useState(listOfUsers)
 
-    function toggleScratchedStatus(card){
+    function toggleScratchedStatus(card) {
 
         //Toggle to scratch and unscratch cards
-        if(card.scratched){
+        if (card.scratched) {
             listOfPeople[card.key].scratched = false
             setListOfPeople([...listOfPeople])
-        }else{
+        } else {
             listOfPeople[card.key].scratched = true
             setListOfPeople([...listOfPeople])
         }
     }
 
     console.log(listOfPeople)
-  return (
-      <div>
-        <Header />
+    return (
+        <div>
+            <Header/>
 
-        {listOfPeople.map(card => (
-            <Card
-                key={card.key}
-                name={card.name}
-                role={card.role}
-                scratched={card.scratched}
-                onClick={() => toggleScratchedStatus(card)}
-            />
-        ))}
+            {listOfPeople.map(card => (
+                <Card
+                    key={card.key}
+                    name={card.name}
+                    role={card.role}
+                    scratched={card.scratched}
+                    onClick={() => toggleScratchedStatus(card)}
+                />
+            ))}
 
-        <Footer />
-      </div>
-  );
+            <div>
+                <p>
+                    {joke}
+                </p>
+            </div>
+
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
