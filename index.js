e9cc3f7e40744ebacf779080872a1e261953f273
JaviAlbe <64216714+JaviAlbe@users.noreply.github.com>
2021-09-13 17:19:18 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-//Challenge. Render all the listOfUsers inside listOfUsers.js as a seperate Card
-//component.
