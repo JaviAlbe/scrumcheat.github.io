9dfa98c8a7524b543cd94fb0d334e925d0fd79f3
JaviAlbe <64216714+JaviAlbe@users.noreply.github.com>
2021-09-13 16:49:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Challenge. Render all the listOfUsers inside listOfUsers.js as a seperate Card
+//component.
